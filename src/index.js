41598229b816993c0a4f53933f914c2812cdd57c
dainntyy <z2507dasha@gmail.com>
2025-02-21 18:16:33 +0000

    fix loading

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+if (!sessionStorage.getItem("hasVisited")) {
+  sessionStorage.setItem("hasVisited", "true");
+  window.location.reload();
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
