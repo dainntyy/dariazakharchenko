3b54b5a4d9340b71da38935e61d0199cfc6fc215
dainntyy <z2507dasha@gmail.com>
2025-07-14 19:00:23 +0100

    Changed first render

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-if (!sessionStorage.getItem("hasVisited")) {
-  sessionStorage.setItem("hasVisited", "true");
-  window.location.reload();
+if (!sessionStorage.getItem("reloaded")) {
+  sessionStorage.setItem("reloaded", "true");
+  window.location.replace(window.location.href);
+} else {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
